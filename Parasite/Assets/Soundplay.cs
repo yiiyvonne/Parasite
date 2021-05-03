fd1517c3169f17e08d6da2331b934d4aec265580
yiiyvonne <d0686014@o365.fcu.edu.tw>
2021-05-03 14:34:05 +0800

    ver2

---
+++ Parasite/Assets/Soundplay.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Soundplay : MonoBehaviour
+{
+    AudioSource source;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+                
+    }
+
+    private void OnCollisionEnter(Collision col)
+    {
+        print("collision");
+
+        if (col.gameObject.CompareTag("sound"))
+        {
+            source.Play();
+        }
+    }
+}
